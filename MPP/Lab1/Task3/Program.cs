9412224efb9bf9f56d1ead766d5de8313a0c968f
vangaru <ivanenko0607@gmail.com>
2021-09-08 08:56:22 -0800

    add: MPP Lab1 Task3

---
+++ MPP/Lab1/Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task3
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            // test
+            var str = Console.ReadLine();
+            Console.WriteLine(StripWhitespaces(str));
+        }
+
+        private static string StripWhitespaces(string str)
+            => string.IsNullOrEmpty(str) ? null : StripNotNullStringWhitespaces(str);
+
+        private static string StripNotNullStringWhitespaces(string str)
+        {
+            if (str[0] == ' ' && str[^1] == ' ') return str[1..^1];
+            if (str[0] == ' ') return str[1..];
+            return str[^1] == ' ' ? str[..^1] : str;
+        }
+    }
+}
