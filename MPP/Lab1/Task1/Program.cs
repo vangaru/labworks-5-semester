ecfcd3634388d65cf44b743fba2461a28d642147
vangaru <ivanenko0607@gmail.com>
2021-09-08 06:53:29 -0800

    add: lab1 MPP

---
+++ MPP/Lab1/Task1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task1
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var nums = ConvertArgsToIntArray(args);
+            TryPrintNumberWithMaxEjectionIfNotEmpty(nums);
+        }
+
+        private static int[] ConvertArgsToIntArray(string[] args) =>
+            args.Length == 1 ? Array.Empty<int>() : args[1..args.Length].Select(int.Parse).ToArray();
+
+        private static void TryPrintNumberWithMaxEjectionIfNotEmpty(int[] nums)
+        {
+            try
+            {
+                var numberWithMaxEjection = GetNumberWithMaxEjectionIfNotEmpty(nums);
+                Console.WriteLine(numberWithMaxEjection);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        
+        private static int GetNumberWithMaxEjectionIfNotEmpty(int[] nums)
+        {
+            if (nums.Length == 0) throw new ArgumentOutOfRangeException(nameof(nums), "Args cannot be empty");
+            var numberEjectionDictionary = GetNumberEjectionDictionary(nums);
+            return numberEjectionDictionary
+                .Aggregate((x, y) => x.Value > y.Value ? x : y).Key;
+        }
+
+        private static Dictionary<int, int> GetNumberEjectionDictionary(IEnumerable<int> arr)
+        {
+            arr = arr.ToList();
+            var numberEjectionDictionary = new Dictionary<int, int>();
+            
+            foreach (var num in arr)
+                numberEjectionDictionary[num] = GetEjection(arr, num);
+            
+            return numberEjectionDictionary;
+        }
+
+        private static int GetEjection(IEnumerable<int> arr, int num)
+        {
+            var ejection = Math.Abs(arr.Sum(i => num - i));
+            return ejection;
+        }
+    }
+}
---
+++ MPP/Lab1/Task3/Program.cs
@@ -0,0 +1,33 @@
+﻿#nullable enable
+using System;
+using System.Linq;
+
+namespace Task1
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine(StripWhiteSpaces("") == null); ;
+            Console.WriteLine(StripWhiteSpaces(" ") == null);
+            Console.WriteLine(StripWhiteSpaces(" a bc"));
+            Console.WriteLine(StripWhiteSpaces(" a bc")!.Length);
+            Console.WriteLine(StripWhiteSpaces(" abc "));
+            Console.WriteLine(StripWhiteSpaces(" abc ")!.Length);
+            Console.WriteLine(StripWhiteSpaces("abc "));
+            Console.WriteLine(StripWhiteSpaces("abc ")!.Length);
+        }
+
+        private static string? StripWhiteSpaces(string? str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return null;
+            }
+            if (str.Length >= 2 && str.First() == ' ' && str.Last() == ' ') return str[1..^1];
+            if (str.First() == ' ') return str[1..];
+            if (str.Last() == ' ') return str[..^1];
+            return str;
+        }
+    }
+}
